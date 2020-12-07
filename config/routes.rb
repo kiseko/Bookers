b19c731cb85545996d06dce687736a3ed3a6a21c
Motoyuki Koseki <motoyuki.koseki@gmail.com>
2020-12-07 21:14:57 +0000

    [update]3-8(投稿機能の追加)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root :to => 'books#top'
+  get "books" => "books#home"
+  post "books" => "books#create"
+  get "books/:id" => "books#show", as: "book"
+  get "books/:id/edit" => "books#edit", as: "edit_book"
+  patch "books/:id" => "books#update", as: "update_book"
+  delete "books/:id" => "books#destroy", as: "delete_book"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
