489f9588c7377008f59a34c16f67a1d6528dd03c
Motoyuki Koseki <motoyuki.koseki@gmail.com>
2020-12-08 20:10:24 +0000

    [Update}3-8(バリデーションチェックの追加)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,9 +8,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :home
+    end
   end
 
   def show
