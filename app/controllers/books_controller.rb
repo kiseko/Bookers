155cde1be75510b49aeb55a9d8200075d7697abd
Motoyuki Koseki <motoyuki.koseki@gmail.com>
2020-12-09 19:51:42 +0000

    [Finish]3-8(RSpecによるテスト:クリア済)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
       redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully created."
     else
       @books = Book.all
       render :home
@@ -26,9 +27,14 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+       flash[:notice] = "Book was successfully updated."
+    else
+      render :edit
+    end  
+      
   end
 
   def destroy
